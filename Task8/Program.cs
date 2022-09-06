aad8ba0e18eec0cca079137eed6a8299044def02
Григорий Васякин <vxdll.39@gmail.com>
2022-09-06 14:37:08 +0200

    Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int i = 0;
+while (count <= num)
+{
+    if (i % 2 == 0)
+    {   
+        Console.WriteLine(i);
+        count = count + 1;
+        i = i + 1;
+    }
+    else
+    {
+        count = count + 1;
+        i = i + 1;
+    }
+}
